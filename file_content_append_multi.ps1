24ac9ad7ef2188c51ec46ba4e38bc463c08a70e4
John <102876985+johndevopsus@users.noreply.github.com>
2023-12-06 12:06:13 -0500

    Update file_content_append_multi.ps1

--- file_content_append_multi.ps1
+++ file_content_append_multi.ps1
@@ -1,59 +1,53 @@
-# Set the directory where the "config.xml" files are located
+# Set the directory path where you want to search for config.xml files
 $directoryPath = "C:\Path\To\Your\Directory"
 
-# Set the list of strings to search for
+# Define a list of strings to search for in the config.xml files
 $searchStrings = @("String1", "String2", "String3")
 
-# Set the dictionary for string replacement (if needed)
-$replacementDictionary = @{
-    "String1" = "NewString1"
-    "String2" = "NewString2"
-    "String3" = "NewString3"
+# Define a dictionary of strings to append after the searched string
+$appendStrings = @{
+    "String1" = "AppendedText1"
+    "String2" = "AppendedText2"
+    "String3" = "AppendedText3"
 }
 
-# Set the output file path
-$outputFilePath = "C:\Path\To\Your\Output\Result.txt"
+# Specify the output file path for the results
+$outputFilePath = "C:\Path\To\Your\OutputFile.txt"
 
 # Initialize an array to store the results
 $results = @()
 
-# Get all "config.xml" files in the directory and its subdirectories
-$configFiles = Get-ChildItem -Path $directoryPath -Filter "config.xml" -Recurse
+# Iterate through each search string
+foreach ($searchString in $searchStrings) {
+    # Find all config.xml files containing the current search string
+    $files = Get-ChildItem -Path $directoryPath -Filter "config.xml" -Recurse | 
+             Select-String -Pattern $searchString | 
+             Select-Object Path, Line
 
-# Iterate through each file
-foreach ($file in $configFiles) {
-    # Read the content of the file
-    $fileContent = Get-Content $file.FullName
+    # Iterate through each result and update the file
+    foreach ($file in $files) {
+        # Get the content of the file
+        $content = Get-Content -Path $file.Path
 
-    # Iterate through each search string
-    foreach ($searchString in $searchStrings) {
-        # Search for the string in the file content
-        $matchingLines = $fileContent | Select-String -Pattern $searchString
+        # Find the line number in the content
+        $lineNumber = $file.LineNumber
 
-        # Iterate through each matching line
-        foreach ($line in $matchingLines) {
-            # Get the line number
-            $lineNumber = $line.LineNumber
+        # Append the new string after the searched string
+        $content[$lineNumber - 1] += " $($appendStrings[$searchString])"
 
-            # Get the line content
-            $lineContent = $line.Line
+        # Update the file with the modified content
+        Set-Content -Path $file.Path -Value $content
 
-            # Add the replacement string if applicable
-            if ($replacementDictionary.ContainsKey($searchString)) {
-                $lineContent += $replacementDictionary[$searchString]
-            }
-
-            # Add the result to the array
-            $result = [PSCustomObject]@{
-                Path = $file.FullName
-                Line = "$lineNumber: $lineContent"
-            }
-
-            $results += $result
+        # Add the result to the array
+        $results += [PSCustomObject]@{
+            Path = $file.Path
+            Line = $file.Line
         }
     }
 }
 
 # Export the results to a text file
 $results | Format-Table -AutoSize | Out-File -FilePath $outputFilePath
-Write-Host "Results saved to: $outputFilePath"
+
+# Display a message indicating the script has completed
+Write-Host "Script completed. Results saved to: $outputFilePath"
